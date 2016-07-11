ee748034b2bbd0e29cee3dc841a60ba9e0667408
ianrahman <ian.alexander.rahman@gmail.com>
2016-07-11 14:56:48 -0400

    Finished solution

--- iOS-Week4-Swift-Assessment/TestViewController.swift
+++ iOS-Week4-Swift-Assessment/TestViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class TestViewController: UIViewController {
+class TestViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    @IBOutlet weak var testTableView: TestTableView! = TestTableView()
+    @IBOutlet weak var testTableView: UITableView! = UITableView()
     
-    var shuffleCounter = 1
+    var testStrings = ["we're", "going", "to", "rock", "down", "to", "electric", "avenue"]
+    // And then we'll take it higher!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,29 +21,64 @@ class TestViewController: UIViewController {
     }
 
     func setupTableView() {
+        self.testTableView.delegate = self
+        self.testTableView.dataSource = self
+        
         self.testTableView.translatesAutoresizingMaskIntoConstraints = false
+        self.testTableView.removeConstraints(self.testTableView.constraints)
         self.testTableView.heightAnchor.constraintEqualToAnchor(self.view.heightAnchor).active = true
         self.testTableView.widthAnchor.constraintEqualToAnchor(self.view.widthAnchor).active = true
-        self.testTableView.centerXAnchor.constraintEqualToAnchor(self.view.centerXAnchor).active = true
         self.testTableView.centerYAnchor.constraintEqualToAnchor(self.view.centerYAnchor).active = true
-        
-        
+        self.testTableView.centerXAnchor.constraintEqualToAnchor(self.view.centerXAnchor).active = true
     }
     
-    @IBAction func colorButtonTapped(sender: AnyObject) {
-        self.view.backgroundColor = getRandomColor()
+
+    
+    func tableView(tableView:UITableView, numberOfRowsInSection section:Int) -> Int {
+        return self.testStrings.count
     }
     
-    func getRandomColor() -> UIColor{
-        let randomRed:CGFloat = CGFloat(drand48())
-        let randomGreen:CGFloat = CGFloat(drand48())
-        let randomBlue:CGFloat = CGFloat(drand48())
-        return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        if let textLabel = cell.textLabel {
+            textLabel.text = self.testStrings[indexPath.row]
+        }
+        
+        return cell
     }
     
     @IBAction func shuffleButtonTapped(sender: AnyObject) {
-        self.testTableView.shuffle()
+        print("Shuffling strings in \(self.testStrings)")
+        var tempArray: Array<String> = []
+        while self.testStrings.count > 0 {
+            let index = Int(arc4random_uniform(UInt32(self.testStrings.count)))
+            tempArray.append(self.testStrings[index])
+            self.testStrings.removeAtIndex(index)
+        }
+        self.testStrings = tempArray
+        print("Result: \(self.testStrings)")
+        self.testTableView.reloadData()
     }
     
+    @IBAction func shiftButtonTapped(sender: AnyObject) {
+        let randomNumber = Int(arc4random_uniform(UInt32(self.testStrings.count)))
+        print("Shift strings in \(self.testStrings)\n \(randomNumber) spaces")
+        self.testStrings = self.shift(randomNumber, strings: self.testStrings)
+        print("Result: \(self.testStrings)")
+        self.testTableView.reloadData()
+    }
+    
+    func shift(spaces: Int, strings: Array<String>) -> Array<String> {
+        var shiftedStrings = strings
+        var i = spaces
+        while i > 0 {
+            if let lastString = shiftedStrings.popLast() {
+                shiftedStrings.insert(lastString, atIndex: 0)
+            }
+            i -= 1
+        }
+        return shiftedStrings
+    }
 }
 
