47b6c1e136c1ae35f0fbc171325b890dc2cd6c80
ianrahman <ian.alexander.rahman@gmail.com>
2016-07-12 10:15:21 -0400

    Removed solution

--- iOS-Week4-Swift-Assessment/ViewController.swift
+++ iOS-Week4-Swift-Assessment/ViewController.swift
@@ -10,59 +10,19 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    @IBOutlet weak var tableView: UITableView!
-    
-    var electricArray = "We're going to rock down to Electric Avenue".componentsSeparatedByString(" ")
-    // And then we'll take it higher!
+    /*
+     
+     * Add your properties here!
+     
+     */
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.delegate = self
-        tableView.dataSource = self
-        setupTableView()
     }
     
-    func setupTableView() {
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.removeConstraints(tableView.constraints)
-        tableView.heightAnchor.constraintEqualToAnchor(view.heightAnchor, multiplier: 0.75).active = true
-        tableView.widthAnchor.constraintEqualToAnchor(view.widthAnchor).active = true
-        tableView.centerXAnchor.constraintEqualToAnchor(view.centerXAnchor).active = true
-        tableView.bottomAnchor.constraintEqualToAnchor(view.bottomAnchor).active = true
-    }
-    
-    func tableView(tableView:UITableView, numberOfRowsInSection section:Int) -> Int {
-        return electricArray.count
-    }
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        
-        if let textLabel = cell.textLabel {
-            textLabel.text = electricArray[indexPath.row]
-        }
-        
-        return cell
-    }
-    
-    @IBAction func shiftButtonTapped(sender: AnyObject) {
-        let randomNumber = Int(arc4random_uniform(UInt32(electricArray.count)))
-        print("Shift strings in \(electricArray)\n \(randomNumber) spaces\n")
-        self.electricArray = shift(randomNumber, strings: electricArray)
-        print("Result: \(electricArray)\n")
-        tableView.reloadData()
-    }
-    
-    func shift(spaces: Int, strings: [String]) -> [String] {
-        var shiftedStrings = strings
-        var i = spaces % strings.count
-        while i > 0 {
-            if let lastString = shiftedStrings.popLast() {
-                shiftedStrings.insert(lastString, atIndex: 0)
-            }
-            i -= 1
-        }
-        return shiftedStrings
-    }
-    
+    /*
+ 
+     * Write your code here!
+ 
+    */
 }
